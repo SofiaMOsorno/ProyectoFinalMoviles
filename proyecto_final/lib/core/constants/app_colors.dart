765dd83d28ec7986c87b84b1821cab585f34c482
Sofi <sofiosorno66@gmail.com>
2025-10-28 12:34:36 -0600

    dark mode

--- proyecto_final/lib/core/constants/app_colors.dart
+++ proyecto_final/lib/core/constants/app_colors.dart
@@ -11,6 +11,7 @@ class AppColors {
   static const Color textField = Color(0xFFFFFFFF);
 
   static const Color borderDarkBlue = Color(0xFF0A1931);
+  static const Color textDarkBlue = Color.fromARGB(255, 139, 177, 240);
 
   AppColors._();
 }
--- proyecto_final/lib/core/theme/theme_provider.dart
+++ proyecto_final/lib/core/theme/theme_provider.dart
@@ -11,13 +11,13 @@ class ThemeProvider extends ChangeNotifier {
   }
 
   // Colores dinámicos según el tema
-  Color get primaryColor => _isDarkMode ? const Color(0xFF1A1A2E) : const Color(0xFF6B1D5C);
-  Color get secondaryColor => _isDarkMode ? const Color(0xFF16213E) : const Color(0xFFFF3366);
-  Color get backgroundColor => _isDarkMode ? const Color(0xFF0F0F1E) : const Color(0xFF0A1931);
+  Color get primaryColor => _isDarkMode ? const Color.fromARGB(255, 1, 1, 61) : const Color(0xFF6B1D5C);
+  Color get secondaryColor => _isDarkMode ? const Color.fromARGB(255, 128, 149, 205) : const Color(0xFFFF3366);
+  Color get backgroundColor => _isDarkMode ? const Color.fromARGB(255, 33, 33, 79) : const Color(0xFF0A1931);
   
   Color get lightAccent => _isDarkMode ? const Color(0xFF6B7AA1) : const Color(0xFF9B7A95);
   
-  Color get textPrimary => const Color(0xFFFFFFFF);
+  Color get textPrimary => _isDarkMode ? const Color.fromARGB(255, 197, 196, 206) :const Color(0xFFFFFFFF);
   Color get textField => _isDarkMode ? const Color(0xFF2E2E3E) : const Color(0xFFFFFFFF);
   
   Color get border => _isDarkMode ? const Color(0xFF16213E) : const Color(0xFF0A1931);
